2ce75d71889c91d07a76b091072374b57793304e
Igor Yegorov <iegoroff@gmail.com>
2018-09-15 14:02:30 +0300

    fable bindings

---
+++ fable-typed/README.md
@@ -0,0 +1,11 @@
+# Fable bindings for [react-native-collapsible-header-views](https://npmjs.com/package/react-native-collapsible-header-views)
+
+[![nuget](https://badge.fury.io/nu/Fable.Import.ReactNativeCollapsibleHeaderViews.svg)](https://badge.fury.io/nu/Fable.Import.ReactNativeCollapsibleHeaderViews)
+
+#### Win
+1. `paket add Fable.Import.ReactNativeCollapsibleHeaderViews`
+2. `npm i react-native-collapsible-header-views`
+
+#### Mono
+1. `mono .paket/paket.exe add Fable.Import.ReactNativeCollapsibleHeaderViews`
+2. `npm i react-native-collapsible-header-views`
---
+++ fable-typed/src/Fable.Import.ReactNativeCollapsibleHeaderViews.fs
@@ -0,0 +1,114 @@
+module Fable.Import.ReactNativeCollapsibleHeaderViews
+
+open Fable.Helpers.React
+open Fable.Import.ReactNative
+open Fable.Helpers.ReactNative.Props
+open Fable.Core
+open Fable.Core.JsInterop
+
+
+module Props =
+
+  type AnimationConfig =
+    { animated: bool }
+  
+  type CollapsibleHeaderProps =
+    { interpolatedHeaderTranslation: (float -> float -> Animated.AnimatedInterpolation)
+      showHeader: (AnimationConfig -> unit)
+      hideHeader: (AnimationConfig -> unit) }
+
+  type CollapsibleHeaderView =
+    inherit React.ComponentClass<ScrollViewProperties>
+    abstract showHeader: (AnimationConfig -> unit) with get, set
+    abstract hideHeader: (AnimationConfig -> unit) with get, set
+    abstract animatedComponent: (unit -> obj option) with get, set
+
+  type CollapsibleHeaderViewProps =
+    | StatusBarHeight of float
+    | HeaderContainerBackgroundColor of string
+    | DisableHeaderSnap of bool
+    | HeaderAnimationDuration of float
+    | Ref of Ref<CollapsibleHeaderView>
+
+
+open Props
+
+let inline collapsibleHeaderScrollView
+  (header: (CollapsibleHeaderProps -> React.ReactElement))
+  (headerHeight: float)
+  (collapsibleProps: CollapsibleHeaderViewProps list)
+  (props: ScrollViewProperties list)
+  (children: React.ReactElement list): React.ReactElement =
+
+  ofImport
+    "CollapsibleHeaderScrollView"
+    "react-native-collapsible-header-views"
+    (JS.Object.assign(
+      createObj
+        [ "CollapsibleHeaderComponent" ==> header
+          "headerHeight" ==> headerHeight ],
+      keyValueList CaseRules.LowerFirst collapsibleProps,
+      keyValueList CaseRules.LowerFirst props
+    ))
+    children
+
+let inline collapsibleHeaderFlatList<'a>
+  (data:'a [])
+  (header: (CollapsibleHeaderProps -> React.ReactElement))
+  (headerHeight: float)
+  (collapsibleProps: CollapsibleHeaderViewProps list)
+  (props: IFlatListProperties<'a> list): React.ReactElement =
+
+  let pascalCaseProps =
+    List.filter (function
+                 | ItemSeparatorComponent _ -> true
+                 | ListEmptyComponent _ -> true
+                 | ListFooterComponent _ -> true
+                 | ListHeaderComponent _ -> true
+                 | _ -> false)
+                 (unbox<FlatListProperties<'a> list> props)
+
+  ofImport
+    "CollapsibleHeaderFlatList"
+    "react-native-collapsible-header-views"
+    (JS.Object.assign(
+      createObj
+        [ "data" ==> data
+          "CollapsibleHeaderComponent" ==> header
+          "headerHeight" ==> headerHeight ],
+      keyValueList CaseRules.LowerFirst collapsibleProps,
+      keyValueList CaseRules.LowerFirst props,
+      keyValueList CaseRules.None pascalCaseProps
+    ))
+    []
+
+let inline collapsibleHeaderSectionList<'a>
+  (sections: SectionListData<'a> [])
+  (header: (CollapsibleHeaderProps -> React.ReactElement))
+  (headerHeight: float)
+  (collapsibleProps: CollapsibleHeaderViewProps list)
+  (props: ISectionListProperties<'a> list): React.ReactElement =
+
+  let pascalCaseProps =
+    List.filter (function
+                 | SectionListProperties.ItemSeparatorComponent _ -> true
+                 | SectionListProperties.ListEmptyComponent _ -> true
+                 | SectionListProperties.ListFooterComponent _ -> true
+                 | SectionListProperties.ListHeaderComponent _ -> true
+                 | SectionSeparatorComponent _ -> true
+                 | _ -> false)
+                 (unbox<SectionListProperties<'a> list> props)
+
+  ofImport
+    "CollapsibleHeaderSectionList"
+    "react-native-collapsible-header-views"
+    (JS.Object.assign(
+      createObj
+        [ "sections" ==> sections
+          "CollapsibleHeaderComponent" ==> header
+          "headerHeight" ==> headerHeight ],
+      keyValueList CaseRules.LowerFirst collapsibleProps,
+      keyValueList CaseRules.LowerFirst props,
+      keyValueList CaseRules.None pascalCaseProps
+    ))
+    []
